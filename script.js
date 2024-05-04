966b64608e3a1f19510ba1d47935085ff880227a
Pradeep-bera <pradeepbera91@gmail.com>
2024-05-05 02:18:26 +0530

    add

---
+++ script.js
@@ -0,0 +1,7 @@
+const listName = document.querySelector('.list-name');
+const listPosition = document.querySelector('.list-position');
+
+let newDiv = document.createElement('div');
+newDiv.innerHTML = "";
+newDiv.classList.add('welcome-card');
+document.container.appendChild(newDiv);
